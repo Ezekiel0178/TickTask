4cf625e48095de00220a1dcfa291d26e3aa138b2
Techzeke <dcfundz54@gmail.com>
2026-03-30 13:06:33 +0100

    Add files via upload

--- auth.js
+++ auth.js
@@ -93,7 +93,13 @@ if(logoutBtn){
     });
 }
 
+// On reload,goes back to the loader
 
+const navEntries = performance.getEntriesByType("navigation");
+
+  if (navEntries.length > 0 && navEntries[0].type === "reload") {
+    window.location.href = "index.html";
+}
 
 
 
